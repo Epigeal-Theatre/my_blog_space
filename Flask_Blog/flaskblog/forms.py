2be572426cb051dcc33b4491260c1d3357b88c1f
Epigeal-Theatre <danielonyinkwa1@gmail.com>
2024-07-10 01:45:34 +0300

    add gmail configuration and password reset auth

--- Flask_Blog/flaskblog/forms.py
+++ Flask_Blog/flaskblog/forms.py
@@ -58,3 +58,20 @@ class PostForm(FlaskForm):
 	content = TextAreaField('Content', validators=[DataRequired()])
 	submit = SubmitField('Post')
 
+class RequestResetForm(FlaskForm):
+	email = StringField('Email', validators=[DataRequired(), Email()])
+	submit = SubmitField('RequestPassword Reset')
+
+	def validate_email(self, email):
+
+		user = User.query.filter_by(email=email.data).first()
+		if user is None:
+			raise ValidationError('An account with that email does not exist. You must Register first')
+
+
+class ResetPasswordForm(FlaskForm):
+	password = PasswordField('Password', validators=[DataRequired()])
+	confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+	submit = SubmitField('Reset Password')
+
+
