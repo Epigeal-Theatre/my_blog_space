64e9b192167d9ab105d9cf109f4471eb14b73739
Epigeal-Theatre <danielonyinkwa1@gmail.com>
2024-07-14 02:49:19 +0300

    restructure files to form blueprints

--- Flask_Blog/flaskblog/__init__.py
+++ Flask_Blog/flaskblog/__init__.py
@@ -1,27 +1,27 @@
-import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_mail import Mail
+from flaskblog.config import Config
 
 app = Flask(__name__)
-
-app.config['SECRET_KEY'] = 'de9c69daa832c747b4c1e4d414387d37'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config.from_object(Config)
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager= LoginManager(app)
-login_manager.login_view = 'login'
+login_manager.login_view = 'users.login'
 login_manager.login_message_category = 'info'
-app.config['MAIL_SERVER'] = 'smtp.gmail.com'
-app.config['MAIL_PORT'] = 587
-app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = 'dantestdevs@gmail.com'
-app.config['MAIL_PASSWORD'] = 'jbtu nath hgqw umqd'
+
 
 mail = Mail(app)
 
 
 
-from flaskblog import routes
+from flaskblog.users.routes import users
+from flaskblog.posts.routes import posts
+from flaskblog.main.routes import main
+
+app.register_blueprint(users)
+app.register_blueprint(posts)
+app.register_blueprint(main)
