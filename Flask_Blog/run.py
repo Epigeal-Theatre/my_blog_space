034662b37897e0f37856bb23a3d6a23aed55618c
Epigeal-Theatre <danielonyinkwa1@gmail.com>
2024-07-14 04:25:51 +0300

    clean proc file by removing conflict code

--- Flask_Blog/run.py
+++ Flask_Blog/run.py
@@ -1,10 +1,6 @@
-import os
-import sys
+from flaskblog import create_app
 
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../')))
-
-
-from flaskblog import app
+app = create_app()
 
 
 if __name__ == '__main__':
--- Procfile
+++ Procfile
@@ -1,5 +1,1 @@
-<<<<<<< HEAD.
-web: gunicorn run:app
-=======.
-web: gunicorn Flask_Blog.run:app 
->>>>>>>.. f95ca3f6da2a7f2ac1a07bbdd6ee53c6bbb6eb72
+web: gunicorn Flask_Blog.run:app
